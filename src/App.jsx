0352398d74a6cf598e02208f22981485f7d312cc
AngelTapiaLedesma <angelitotapia10@gmail.com>
2024-07-16 22:19:04 -0600

    React App

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Header>
       </Header>
-      <Banner></Banner>
+      <Banner/>
     </>
   )
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,21 @@
-import "./Card.css"
-import player from "../../../public/img/player.jpg"
+import "./Card.css";
+import player from "../../../public/img/player.jpg";
 import Title from "../Title";
+
 const Card = () => {
     return(
         <>
             <div className="card">
-                <Title title="FrontEnd"></Title>
-                <div>
-                    <h2>Challenge React</h2>
-                    <p>Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte en la resolución de un problema para poder aplicar todos los conocimientos adquiridos en la formación React.</p>
+                <div className="card-content">
+                    <Title title="FrontEnd" />
+                    <h2 className="subtitle">Challenge React</h2>
+                    <p className="paragraph">Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte en la resolución de un problema para poder aplicar todos los conocimientos adquiridos en la formación React.</p>
                 </div>
-            </div>
-            <img src={player} alt="imagen de alura" />
-        </>
-    )
 
+                <img className="image" src={player} alt="imagen de alura" />
+            </div>
+        </>   
+    )
 }
 
 export default Card;
